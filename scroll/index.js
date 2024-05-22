00b7168a00c7d5b2e21aca268be8d9733b7502da
Gayatri Khodke <gayatrikhodke22@navgurukul.org>
2024-05-22 17:01:19 +0530

    modify scroll file

--- scroll/index.js
+++ scroll/index.js
@@ -15,3 +15,30 @@ window.addEventListener('scroll',function(){
         nav.classList.remove('fix-nav');
     }
 })
+let linkparent=document.querySelector('.list1');
+let scrollLinks=document.querySelectorAll('.scroll-link');
+scrollLinks.forEach(function(link){
+    link.addEventListener('click',function(e){
+        e.preventDefault();
+        let id=e.currentTarget.getAttribute('href').slice(1);
+        let element=document.getElementById(id);
+
+        // calculate our navbar height and scroll window height
+        const containerHeight = linkparent.getBoundingClientRect().height;
+        const fixedNav = nav.classList.contains("fix-nav");
+        let navHeight=nav.getBoundingClientRect().height;
+        let position=element.offsetTop-navHeight;
+        if (!fixedNav) {
+            position = position - navHeight;
+          }
+          if (navHeight > 82) {
+            position = position + containerHeight;
+          }
+      
+          window.scrollTo({
+            left: 0,
+            top: position,
+          });
+          linkparent.style.height = 3;
+    })
+})
