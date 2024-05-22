e9e013e951fcbe78494e86d2f9c4795d06282e82
Gayatri Khodke <gayatrikhodke22@navgurukul.org>
2024-05-22 15:51:15 +0530

    modify scroll page

--- scroll/index.js
+++ scroll/index.js
@@ -3,3 +3,15 @@ icon.addEventListener('click', function() {
     let list1 = document.querySelector('.list1');
     list1.classList.toggle('show');
 });
+
+let nav=document.querySelector('.nav');
+window.addEventListener('scroll',function(){
+    let scrollHeight=window.pageYOffset;
+    let navHeight=nav.getBoundingClientRect().height;
+    if(scrollHeight>navHeight){
+        nav.classList.add('fix-nav');
+    }
+    else{
+        nav.classList.remove('fix-nav');
+    }
+})
