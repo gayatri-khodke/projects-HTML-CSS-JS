630a53efb2dec0c77c77b622deaf4239462962e0
Gayatri Khodke <gayatrikhodke22@navgurukul.org>
2024-05-31 10:12:41 +0530

    modify weather app

--- weatherAPI/index.js
+++ weatherAPI/index.js
@@ -1,6 +1,7 @@
 let input=document.querySelector('input');
 let submit=document.querySelector('.btn');
 let output=document.querySelector('.tempeature');
+let city=document.querySelector('.city');
 submit.addEventListener('click',getweather);
 let key='82aa1d2dd6678385fffe5dcd3d3f7a58';
 
@@ -11,11 +12,26 @@ function getweather(){
     })
     .then((result)=>{
         console.log(result);
-        output.innerHTML=`${result.name} ${result.main.temp} C`;
+        city.innerText=`${result.name}`;
+        output.innerHTML=`${result.main.temp} C`;
+        let temp=result.main.temp;
+        checkbg(temp);
     })
     .catch((error)=>{
         console.log(error);
     })
+}
+
+function checkbg(celsious){
+    if (celsious>39){
+        document.body.style.backgroundImage='url("images/bg3.jpeg")';
+    }
+    else if (celsious>30){
+        document.body.style.backgroundImage='url("images/bg1.jpeg")';
+    }
+    else{
+        document.body.style.backgroundImage='url("images/bg5.jpg")';
+    }
 }
 
 
